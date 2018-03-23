a1d34b47062992800a030a9ea4e0d99077cbcb41
smilingkite <mail@katinkahesselink.net>
2018-03-23 10:28:48 +0100

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,41 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+
+          <h1 className="App-title">Calculator</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <table class="tg">
+          <tr>
+            <th class="tg-yw4l">C</th>
+            <th class="tg-yw4l">/</th>
+            <th class="tg-yw4l">X</th>
+            <th class="tg-yw4l">back</th>
+          </tr>
+          <tr>
+            <td class="tg-yw4l">7</td>
+            <td class="tg-yw4l">8</td>
+            <td class="tg-yw4l">9</td>
+            <td class="tg-yw4l">-</td>
+          </tr>
+          <tr>
+            <td class="tg-yw4l">4</td>
+            <td class="tg-yw4l">5</td>
+            <td class="tg-yw4l">6</td>
+            <td class="tg-yw4l">+</td>
+          </tr>
+          <tr>
+            <td class="tg-yw4l">3</td>
+            <td class="tg-yw4l">2</td>
+            <td class="tg-yw4l">1</td>
+            <td class="tg-yw4l">()</td>
+          </tr>
+          <tr>
+            <td class="tg-yw4l">0</td>
+            <td class="tg-yw4l">.</td>
+            <td class="tg-yw4l">+/-</td>
+            <td class="tg-yw4l">=</td>
+          </tr>
+        </table>
       </div>
     );
   }
